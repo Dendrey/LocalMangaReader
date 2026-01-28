cc993f20573ea245643f465a5a8181ab807bfd5e
Dendrey <andrievich.dd@phystech.edu>
2026-01-28 18:16:46 +0700

    Добавил room-runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,17 @@ android {
 
 dependencies {
     implementation(libs.coil.compose) // для отображения изображений
+    implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

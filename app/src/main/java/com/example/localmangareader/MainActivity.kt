1a02aa80625feb27fb18e5922dae546a94c11627
Dendrey <dedeatovich@gmail.com>
2026-02-10 00:04:02 +0300

    убрал лишние зависимости

--- app/src/main/java/com/example/localmangareader/MainActivity.kt
+++ app/src/main/java/com/example/localmangareader/MainActivity.kt
@@ -4,15 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import com.example.localmangareader.ui.components.MangaLibraryTopAppBar
-import com.example.localmangareader.ui.screens.LibraryScreen
 import com.example.localmangareader.ui.screens.MainScreen
 import com.example.localmangareader.ui.theme.LocalMangaReaderTheme
 

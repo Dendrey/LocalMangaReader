daa9773d0e35f044b84093e0e0b6a69506ff5c9e
Dendrey <andrievich.dd@phystech.edu>
2026-02-10 13:18:09 +0300

    добавил иконки

--- app/src/main/java/com/example/localmangareader/ui/screens/ManiScreen.kt
+++ app/src/main/java/com/example/localmangareader/ui/screens/ManiScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +21,7 @@ import com.example.localmangareader.ui.components.MangaLibraryTopAppBar
 @Composable
 @Preview
 fun MainScreen() {
-    var currentScreen by remember { mutableStateOf<AppScreen>(AppScreen.Library) }
+    var currentScreen by rememberSaveable { mutableStateOf<AppScreen>(AppScreen.Library) }
 
     Scaffold(
         topBar = {
